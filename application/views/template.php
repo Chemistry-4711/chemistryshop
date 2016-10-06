7219eb21252f23af4396fc7e88ba72f1f50eb1cb
Kevin Le <kevin-le111@hotmail.com>
2016-10-06 16:50:09 -0700

    removed footer links, recipes controller
    Signed-off-by: Kevin Le <kevin-le111@hotmail.com>

--- application/views/template.php
+++ application/views/template.php
@@ -59,14 +59,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 
 <!-- Footer -->
 <footer class="w3-container w3-padding-64 w3-center w3-opacity">
-  <div class="w3-xlarge w3-padding-32">
-   <a href="#" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
-   <a href="#" class="w3-hover-text-red"><i class="fa fa-pinterest-p"></i></a>
-   <a href="#" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
-   <a href="#" class="w3-hover-text-grey"><i class="fa fa-flickr"></i></a>
-   <a href="#" class="w3-hover-text-indigo"><i class="fa fa-linkedin"></i></a>
- </div>
- <p>Powered by <a href="http://www.w3schools.com/w3css/default.asp" target="_blank">The Chemistry Shop</a></p>
+
+ <p>Powered by <a href="" target="_blank">The Chemistry Shop</a></p>
+ 
 </footer>
 
 <script>
