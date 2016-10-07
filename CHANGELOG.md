c1819b72d7d41825986fcbc9802bf9cb48dbd378
inqr <>
2016-10-06 18:10:34 -0700

    added sales_order and sales_menu views. also updates Sales controller to match Signed-off-by:inqr

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,4 @@ CHANGELOG
 - added Stock model - BC
 - fixed nav bar titles - KL
 - added Homepage controller and remove Welcome controller (updated routes.php accordingly)
+- added sales_order and sales_menu views. also updates Sales controller to match
--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -11,19 +11,22 @@ class Sales extends Application{
     }
     
     public function index() {
-            $this->load->model('stock');
-            $this->data['pagebody'] = 'welcome_message';
+            //$this->load->model('stock');
+            $this->data['pagebody'] = 'sales_menu'; // which view to display on
             
             $source = $this->stock->all();
-            $stock = array ();
             
-            foreach ($source as $row)  {
-                $stock[] = array ('id' => $row['id']);
-            }
-            
-            $this->data['stock'] = $stock;            
+            $this->data['stock'] = $source;    // allows use variables in views        
             $this->render();
     }
     
-    
+    //subcontroller
+    public function get($id){
+        $this->data['pagebody'] = 'sales_order';
+       
+        $item = $this->stock->get($id);
+        
+        $this->data = array_merge($this->data, $item);
+        $this->render();
+    }
 }
